ee423961eb9296d7bb52bb5246cc053809b0c478
takatoshi suzuki <tktc_szk@yahoo.co.jp>
2025-02-22 15:08:34 +0900

    first commit

---
+++ src/diagonal/diagonal.py
@@ -0,0 +1,326 @@
+import pyxel
+
+GAME_TITLE = "DIAGONAL"
+WINDOW_WIDTH = 100
+WINDOW_HEIGHT = 100
+DIAGONAL_UP = 1
+DIAGONAL_DN = -1
+
+SIDE_LEFT = -1
+SIDE_RIGHT = 1
+
+BLOCK_SIZE = 10
+OFFSET_X = 10
+OFFSET_Y = 10
+TABLE_SIZE = 8
+
+
+class Block:
+    def __init__(self, row, col, diagonal):
+        self.row = row
+        self.col = col
+        self.diagonal = diagonal
+        self.x = OFFSET_X + (self.col * BLOCK_SIZE)
+        self.y = OFFSET_Y + (self.row * BLOCK_SIZE)
+        self.selected = False
+        self.right = False
+        self.left = False
+
+    def update(self):
+        pass
+
+    def draw(self):
+        if self.diagonal == DIAGONAL_UP:
+            pyxel.line(
+                self.x + 1,
+                self.y + BLOCK_SIZE - 1,
+                self.x + BLOCK_SIZE - 1,
+                self.y + 1,
+                7,
+            )
+        else:
+            pyxel.line(
+                self.x + 1,
+                self.y + 1,
+                self.x + BLOCK_SIZE - 1,
+                self.y + BLOCK_SIZE - 1,
+                7,
+            )
+
+        if self.selected:
+            pyxel.rectb(
+                self.x,
+                self.y,
+                BLOCK_SIZE + 1,
+                BLOCK_SIZE + 1,
+                2,
+            )
+
+        if self.left:
+            if self.diagonal == DIAGONAL_UP:
+                pyxel.tri(
+                    self.x + 1,
+                    self.y + 1,
+                    self.x + BLOCK_SIZE - 2,
+                    self.y + 1,
+                    self.x + 1,
+                    self.y + BLOCK_SIZE - 2,
+                    3,
+                )
+            else:
+                pyxel.tri(
+                    self.x + 1,
+                    self.y + 2,
+                    self.x + 1,
+                    self.y + BLOCK_SIZE - 1,
+                    self.x + BLOCK_SIZE - 2,
+                    self.y + BLOCK_SIZE - 1,
+                    3,
+                )
+        if self.right:
+            if self.diagonal == DIAGONAL_UP:
+                pyxel.tri(
+                    self.x + 2,
+                    self.y + BLOCK_SIZE - 1,
+                    self.x + BLOCK_SIZE - 1,
+                    self.y + BLOCK_SIZE - 1,
+                    self.x + BLOCK_SIZE - 1,
+                    self.y + 2,
+                    3,
+                )
+            else:
+                pyxel.tri(
+                    self.x + 2,
+                    self.y + 1,
+                    self.x + BLOCK_SIZE - 1,
+                    self.y + 1,
+                    self.x + BLOCK_SIZE - 1,
+                    self.y + BLOCK_SIZE - 2,
+                    3,
+                )
+
+    def select(self, flag):
+        self.selected = flag
+
+
+class Diagonal:
+    def __init__(self):
+        pyxel.init(WINDOW_WIDTH, WINDOW_HEIGHT, title=GAME_TITLE, fps=10)
+        self.is_title = True
+        self.is_gameover = False
+        self.score = 0
+        self.table = []
+        self.cursor_row = 0
+        self.cursor_col = 0
+
+    def reset_game(self):
+        self.score = 0
+        self.table = []
+        for rowIdx in range(TABLE_SIZE):
+            row = []
+            for colIdx in range(TABLE_SIZE):
+                b = Block(
+                    rowIdx,
+                    colIdx,
+                    DIAGONAL_UP if pyxel.rndi(1, 2) == 1 else DIAGONAL_DN,
+                )
+                row.append(b)
+            self.table.append(row)
+        self.cursor_row = 0
+        self.cursor_col = 0
+
+    def update(self):
+        if self.is_title or self.is_gameover:
+            if pyxel.btnp(pyxel.KEY_UP) or pyxel.btnp(pyxel.GAMEPAD1_BUTTON_DPAD_UP):
+                self.is_title = False
+                self.is_gameover = False
+                self.reset_game()
+            return
+        else:
+            if pyxel.btn(pyxel.KEY_LEFT):
+                if self.cursor_col == 0:
+                    self.cursor_col = TABLE_SIZE - 1
+                else:
+                    self.cursor_col -= 1
+            if pyxel.btn(pyxel.KEY_RIGHT):
+                if self.cursor_col == TABLE_SIZE - 1:
+                    self.cursor_col = 0
+                else:
+                    self.cursor_col += 1
+            if pyxel.btn(pyxel.KEY_UP):
+                if self.cursor_row == 0:
+                    self.cursor_row = TABLE_SIZE - 1
+                else:
+                    self.cursor_row -= 1
+            if pyxel.btn(pyxel.KEY_DOWN):
+                if self.cursor_row == TABLE_SIZE - 1:
+                    self.cursor_row = 0
+                else:
+                    self.cursor_row += 1
+
+            if pyxel.btn(pyxel.KEY_A):
+                self.propagete(self.cursor_row, self.cursor_col, SIDE_LEFT)
+            if pyxel.btn(pyxel.KEY_B):
+                self.propagete(self.cursor_row, self.cursor_col, SIDE_RIGHT)
+
+        for col in range(TABLE_SIZE):
+            for row in range(TABLE_SIZE):
+                self.table[row][col].select(
+                    (row, col) == (self.cursor_row, self.cursor_col)
+                )
+
+    def propagete(self, row, col, side):
+        print(row, col, side)
+
+        if side == SIDE_LEFT:
+            if self.table[row][col].left:
+                return
+            else:
+                self.table[row][col].left = True
+
+        if side == SIDE_RIGHT:
+            if self.table[row][col].right:
+                return
+            else:
+                self.table[row][col].right = True
+
+        if side == SIDE_LEFT and self.table[row][col].left:
+            if self.table[row][col].diagonal == DIAGONAL_UP:
+                # up-left
+                if row > 0:
+                    self.propagete(
+                        row - 1,
+                        col,
+                        (
+                            SIDE_RIGHT
+                            if self.table[row - 1][col].diagonal == DIAGONAL_UP
+                            else SIDE_LEFT
+                        ),
+                    )
+                if col > 0:
+                    self.propagete(row, col - 1, SIDE_RIGHT)
+            else:
+                # down-left
+                if row < TABLE_SIZE - 1:
+                    self.propagete(
+                        row + 1,
+                        col,
+                        (
+                            SIDE_LEFT
+                            if self.table[row + 1][col].diagonal == DIAGONAL_UP
+                            else SIDE_RIGHT
+                        ),
+                    )
+                if col > 0:
+                    self.propagete(row, col - 1, SIDE_RIGHT)
+
+        if side == SIDE_RIGHT and self.table[row][col].right:
+            if self.table[row][col].diagonal == DIAGONAL_UP:
+                # down-right
+                if row < TABLE_SIZE - 1:
+                    self.propagete(
+                        row + 1,
+                        col,
+                        (
+                            SIDE_LEFT
+                            if self.table[row + 1][col].diagonal == DIAGONAL_UP
+                            else SIDE_RIGHT
+                        ),
+                    )
+                if col < TABLE_SIZE - 1:
+                    self.propagete(row, col + 1, SIDE_LEFT)
+            else:
+                # up-right
+                if row > 0:
+                    self.propagete(
+                        row - 1,
+                        col,
+                        (
+                            SIDE_RIGHT
+                            if self.table[row - 1][col].diagonal == DIAGONAL_UP
+                            else SIDE_LEFT
+                        ),
+                    )
+                if col < TABLE_SIZE - 1:
+                    self.propagete(row, col + 1, SIDE_LEFT)
+
+    def draw(self):
+        pyxel.cls(0)
+
+        if self.is_title:
+            self.draw_title()
+            return
+
+        if self.is_gameover:
+            self.draw_gameover()
+            return
+
+        self.draw_score()
+        #self.draw_debug()
+        self.draw_lattice()
+
+        for i in range(TABLE_SIZE):
+            for j in range(TABLE_SIZE):
+                self.table[i][j].draw()
+
+    def draw_title(self):
+        for i in range(1, -1, -1):
+            color = 10 if i == 0 else 8
+            title_x = WINDOW_WIDTH / 2 - (len(GAME_TITLE) / 2) * 4
+            title_y = WINDOW_HEIGHT / 2 - 2
+
+            pyxel.text(title_x, 20 + i, GAME_TITLE, color)
+        message = "- Press UP Key -"
+        message_x = WINDOW_WIDTH / 2 - (len(message) / 2) * 4
+
+        pyxel.text(message_x, 40 + i, message, 3)
+
+    def draw_gameover(self):
+        for i in range(1, -1, -1):
+            color = 10 if i == 0 else 8
+            title_x = WINDOW_WIDTH / 2 - (len(GAME_TITLE) / 2) * 4
+            pyxel.text(title_x, 20 + i, GAME_TITLE, color)
+
+        message = "GAME OVER"
+        message_x = WINDOW_WIDTH / 2 - (len(message) / 2) * 4
+
+        pyxel.text(message_x, 30 + i, message, 2)
+
+        message = "- Press Up Key -"
+        message_x = WINDOW_WIDTH / 2 - (len(message) / 2) * 4
+
+        pyxel.text(message_x, 40 + i, message, 3)
+
+    def draw_score(self):
+        fscore = f"SCORE:{self.score:04}"
+        for i in range(1, -1, -1):
+            color = 7 if i == 0 else 0
+            pyxel.text(3 + i, 3, fscore, color)
+
+    def draw_debug(self):
+        debug = f"row:col={self.cursor_row:02}:{self.cursor_col:02}"
+        for i in range(1, -1, -1):
+            color = 7 if i == 0 else 0
+            pyxel.text(3, WINDOW_HEIGHT - 7 + i, debug, color)
+
+    def draw_lattice(self):
+        for i in range(TABLE_SIZE + 1):
+            pyxel.line(
+                OFFSET_X,
+                OFFSET_Y + i * BLOCK_SIZE,
+                OFFSET_X + BLOCK_SIZE * TABLE_SIZE,
+                OFFSET_Y + i * BLOCK_SIZE,
+                7,
+            )
+
+            pyxel.line(
+                OFFSET_X + i * BLOCK_SIZE,
+                OFFSET_Y,
+                OFFSET_X + i * BLOCK_SIZE,
+                OFFSET_Y + BLOCK_SIZE * TABLE_SIZE,
+                7,
+            )
+
+
+diagonal = Diagonal()
+pyxel.run(diagonal.update, diagonal.draw)
---
+++ src/sushi/sushi.py
@@ -0,0 +1,268 @@
+import pyxel
+import math
+
+GAME_TITLE = "SUSHI"
+WINDOW_WIDTH = 100
+WINDOW_HEIGHT = 100
+INITIAL_COUNT = 4
+COLOR_CHILD = 0xF
+COLOR_ADULT = 0x2
+COLOR_SICK = 0x8
+COLOR_PLAYER = 0x7
+
+
+def cmp(a, b):
+    if a == b:
+        return 0
+    else:
+        return 1 if a - b > 0 else -1
+
+
+def bit(v, flg):
+    return 1 if v & flg == flg else 0
+
+
+def distance(a, b):
+    return math.sqrt((b.x - a.x) ** 2 + (b.y - a.y) ** 2)
+
+
+def direction(a, b, leave=False):
+    return cmp(a.x, b.x) * (-1 if leave else 1), cmp(a.y, b.y) * (-1 if leave else 1)
+
+
+def is_overlapping(a, b):
+    x_overlap = a.x <= b.x <= a.x + a.size or a.x < b.x + b.size <= a.x + a.size
+    y_overlap = a.y <= b.y <= a.y + a.size or a.y < b.y + b.size <= a.y + a.size
+
+    return x_overlap and y_overlap
+
+
+class SushiGame:
+    def __init__(self):
+        pyxel.init(WINDOW_WIDTH, WINDOW_HEIGHT, title=GAME_TITLE, fps=10)
+        pyxel.load("sushi.pyxres")
+        self.init_sound()
+        self.is_title = True
+        self.is_gameover = False
+        self.sushi_list = []
+        self.player = None
+        self.score = 0
+        self.hit = False
+        self.reset_game()
+
+        pyxel.run(self.update, self.draw)
+
+    def init_sound(self):
+        # Set sound effects
+        pyxel.sounds[0].set("a3a2c1a1", "p", "7", "s", 5)
+        pyxel.sounds[1].set("f3e2a1a1", "n", "7742", "s", 10)
+
+    def reset_game(self):
+        self.player = Player(self, WINDOW_WIDTH / 2 - 2, WINDOW_HEIGHT / 2 - 2)
+        self.sushi_list = [Sushi(self) for i in range(INITIAL_COUNT)]
+        self.score = 0
+
+    def update(self):
+        self.hit = False
+
+        if self.is_title or self.is_gameover:
+            if pyxel.btnp(pyxel.KEY_UP) or pyxel.btnp(pyxel.GAMEPAD1_BUTTON_DPAD_UP):
+                self.is_title = False
+                self.is_gameover = False
+                self.reset_game()
+            return
+        else:
+            if pyxel.btn(pyxel.KEY_UP) or pyxel.btn(pyxel.GAMEPAD1_BUTTON_DPAD_UP):
+                self.player.up()
+            if pyxel.btn(pyxel.KEY_DOWN) or pyxel.btn(pyxel.GAMEPAD1_BUTTON_DPAD_DOWN):
+                self.player.down()
+            if pyxel.btn(pyxel.KEY_RIGHT) or pyxel.btn(
+                pyxel.GAMEPAD1_BUTTON_DPAD_RIGHT
+            ):
+                self.player.right()
+            if pyxel.btn(pyxel.KEY_LEFT) or pyxel.btn(pyxel.GAMEPAD1_BUTTON_DPAD_LEFT):
+                self.player.left()
+
+            if pyxel.btnp(pyxel.KEY_A) or pyxel.btnp(pyxel.GAMEPAD1_BUTTON_A):
+                self.player.speedup(True)
+            else:
+                self.player.speedup(False)
+
+        self.player.update()
+
+        for sushi in self.sushi_list:
+            sushi.update()
+
+        self.sushi_list = [sushi for sushi in self.sushi_list if sushi.alive]
+
+        for sushi in self.sushi_list:
+            if is_overlapping(self.player, sushi):
+                if sushi.spoiled:
+                    self.score -= 1
+                    pyxel.play(1, 1)
+                    sushi.alive = False
+                    self.hit = True
+                    break
+                else:
+                    self.score += 1
+                    pyxel.play(0, 1)
+                    sushi.alive = False
+                    gain = pyxel.rndi(1, 4)
+                    for i in range(gain):
+                        self.sushi_list.append(Sushi(self))
+                    break
+
+        if len(self.sushi_list) <= 0:
+            self.is_gameover = True
+
+        if not self.player.alive:
+            self.is_gameover = True
+
+    def draw(self):
+        if self.hit:
+            pyxel.cls(2)
+        else:
+            pyxel.cls(0)
+
+        if self.is_title:
+            self.draw_title()
+            return
+
+        if self.is_gameover:
+            self.draw_gameover()
+            return
+
+        self.draw_score()
+
+        for sushi in self.sushi_list:
+            sushi.draw()
+        self.player.draw()
+
+    def draw_title(self):
+        for i in range(1, -1, -1):
+            color = 10 if i == 0 else 8
+            title_x = WINDOW_WIDTH / 2 - (len(GAME_TITLE) / 2) * 4
+            title_y = WINDOW_HEIGHT / 2 - 2
+
+            pyxel.text(title_x, 20 + i, GAME_TITLE, color)
+        message = "- Press UP Key -"
+        message_x = WINDOW_WIDTH / 2 - (len(message) / 2) * 4
+
+        pyxel.text(message_x, 40 + i, message, 3)
+
+    def draw_gameover(self):
+        for i in range(1, -1, -1):
+            color = 10 if i == 0 else 8
+            title_x = WINDOW_WIDTH / 2 - (len(GAME_TITLE) / 2) * 4
+            pyxel.text(title_x, 20 + i, GAME_TITLE, color)
+
+        message = "GAME OVER"
+        message_x = WINDOW_WIDTH / 2 - (len(message) / 2) * 4
+
+        pyxel.text(message_x, 30 + i, message, 2)
+
+        message = "- Press Up Key -"
+        message_x = WINDOW_WIDTH / 2 - (len(message) / 2) * 4
+
+        pyxel.text(message_x, 40 + i, message, 3)
+
+    def draw_score(self):
+        fscore = f"SCORE:{self.score:04}"
+        for i in range(1, -1, -1):
+            color = 7 if i == 0 else 0
+            pyxel.text(3 + i, 3, fscore, color)
+
+
+class Player:
+    def __init__(self, game, x=None, y=None):
+        self.game = (game,)
+        self.x = x if x else pyxel.rndi(0, WINDOW_WIDTH)
+        self.y = y if y else pyxel.rndi(0, WINDOW_HEIGHT)
+        self.speed = 2
+        self.size = 8
+        self.costume = 1
+        self.alive = True
+
+    def update(self):
+        self.costume = self.costume + 1
+        if self.costume > 4:
+            self.costume = 1
+
+    def speedup(self, flag):
+        self.speed = 7 if flag else 2
+
+    def move2(self, x, y):
+        self.x = self.x + self.speed * x
+        self.y = self.y + self.speed * y
+
+    def up(self):
+        self.move2(0, -1)
+
+    def down(self):
+        self.move2(0, 1)
+
+    def right(self):
+        self.move2(1, 0)
+
+    def left(self):
+        self.move2(-1, 0)
+
+    def draw(self):
+        pyxel.blt(self.x, self.y, 0, 16, (self.costume % 2) * 8, 8, 8, 0)
+
+
+class Sushi:
+    def __init__(self, game, x=None, y=None):
+        self.game = game
+        self.x = x if x else pyxel.rndi(0, WINDOW_WIDTH)
+        self.y = y if y else pyxel.rndi(0, WINDOW_HEIGHT)
+        self.speed = pyxel.rndi(1, 3)
+        self.direction_x = pyxel.rndi(1, 3) - 2
+        self.direction_y = pyxel.rndi(1, 3) - 2
+        self.alive = True
+        self.spoiled = False
+        self.size = 6
+        self.age = 0
+        self.spoil = pyxel.rndi(20, 50) * self.speed
+        self.life = self.spoil + pyxel.rndi(15, 30)
+
+    def change_direction(self):
+        if self.direction_x == -1:
+            if self.x <= 3:
+                self.direction_x = 1
+        if self.direction_x == 1:
+            if WINDOW_WIDTH - 6 < self.x:
+                self.direction_x = -1
+        if self.direction_y == -1:
+            if self.y <= 3:
+                self.direction_y = 1
+        if self.direction_y == 1:
+            if WINDOW_HEIGHT - 6 < self.y:
+                self.direction_y = -1
+
+    def move(self):
+        self.x = self.x + (self.speed * self.direction_x)
+        self.y = self.y + (self.speed * self.direction_y)
+
+    def update(self):
+        self.age += 1
+        if self.age % (5 - self.speed) != 0:
+            return
+        self.change_direction()
+        self.move()
+        if self.age > self.spoil:
+            self.spoiled = True
+            self.direction_x = 0
+            self.direction_y = 0
+
+        if self.age > self.life:
+            self.alive = False
+
+    def draw(self):
+        if self.spoiled:
+            pyxel.blt(self.x, self.y, 0, 9, (self.age % 4) * 8, 6, 8, 0)
+        else:
+            pyxel.blt(self.x, self.y, 0, 0, (self.age % 3) * 3, 8, 3, 0)
+
+
+SushiGame()
