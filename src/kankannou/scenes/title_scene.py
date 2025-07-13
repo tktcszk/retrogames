3b4ca5bfea1c0f8c1a4d1d85eafd1f5294411ab8
takatoshi suzuki <tktc_szk@yahoo.co.jp>
2025-07-13 15:07:02 +0900

    20250713

--- src/kankannou/scenes/title_scene.py
+++ src/kankannou/scenes/title_scene.py
@@ -12,11 +12,11 @@ class TitleScene:
         pass
 
     def update(self):
-        if pyxel.btnp(pyxel.KEY_UP):
+        if pyxel.btnp(pyxel.KEY_UP) or pyxel.btnp(pyxel.GAMEPAD1_BUTTON_DPAD_UP):
             self.game.subtitle_idx = (self.game.subtitle_idx - 1) % len(self.game.subtitles)
-        if pyxel.btnp(pyxel.KEY_DOWN):
+        if pyxel.btnp(pyxel.KEY_DOWN) or pyxel.btnp(pyxel.GAMEPAD1_BUTTON_DPAD_DOWN):
             self.game.subtitle_idx = (self.game.subtitle_idx + 1) % len(self.game.subtitles)
-        if pyxel.btnp(pyxel.KEY_A):
+        if pyxel.btnp(pyxel.KEY_A) or pyxel.btnp(pyxel.GAMEPAD1_BUTTON_A):
             self.game.notify(self, "start")
 
     def draw(self):
